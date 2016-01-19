b3f5bf755ccd9ba443892217ace0fc7585eeeca2
Tim Channell <tim.channell@gmail.com>
2016-01-19 16:59:59 -0500

    fixed bug with cleanup

--- test/multilove.test.js
+++ test/multilove.test.js
@@ -8,13 +8,13 @@ var cpus = require('os').cpus().length;
 test('multilove -- creates worker for each cpu', function (t) {
   var stream = multilove({});
   t.equal(stream.workers.length, cpus, 'creates workers equal to number of cpus');
-  stream.end();
+  stream.emit('end');
   t.end();
 });
 
 test('multilove -- number of workers can be manually set', function (t) {
   var stream = multilove({workers: 1});
   t.equal(stream.workers.length, 1, 'limits workers to specified number');
-  stream.end();
+  stream.emit('end');
   t.end();
 });
