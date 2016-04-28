e6b8b288d1e2ca702c4a7579c88ff7eba03ec782
Tim Channell <tim.channell@gmail.com>
2016-04-28 12:30:01 -0400

    fix message queueing, post node-4 callbacks to prevent overflowing messages

--- test/multilove.test.js
+++ test/multilove.test.js
@@ -19,22 +19,3 @@ test('multilove -- number of workers can be manually set', function (t) {
   t.end();
 });
 
-test('multilove -- sends work in batches', function (t) {
-  var stream = multilove({workers: 1, batchSize: 2});
-  var called = 0;
-  var batches = [];
-  stream.on('map', function (dd) {
-    called++;
-    batches.push(dd);
-  }).on('finish', function () {
-    stream.emit('end');
-    t.deepEqual(batches, [[0, 1], [2, 3]], 'sent expected batches');
-    t.equal(called, 2, 'sent expected number of batches');
-    t.end();
-  });
-
-  for (var i = 0; i < 4; i++) {
-    stream.write(i);
-  }
-  stream.end();
-});
--- test/worker.test.js
+++ test/worker.test.js
@@ -4,33 +4,26 @@ var test = require('tap').test,
   worker = require('../lib/worker'),
   Emitter = require('events');
 
-test('work-handler -- extend', function (t) {
-  var custom = worker.extend({
-    callMap: function () {
-      return 5;
-    }
-  });
+var isOldNode = process.versions.node.split('.')[0] < 4;
 
-  t.equal(custom.callMap(), 5, 'new method overrides original');
-  t.notEqual(custom.callMap, worker.callMap, 'original was not mutated');
-  t.end();
-});
+test('work-handler -- `done` sends done messages', function (t) {
+  var w = worker();
 
-test('work-handler -- `done` sends data messages', function (t) {
   var lastSent = null;
 
   var bus = {
-    send: function (message) {
+    send: function (message, cb) {
       lastSent = message;
+      if (!isOldNode) cb();
     },
     on: function () {}
   };
-  worker._install(bus, {}, {});
+  w._install(bus, {}, {});
 
-  worker.done(null, 'Success, Data');
-  t.deepEqual(lastSent, {type: 'data', msg: 'Success, Data'}, 'properly sends "data" message');
+  w.done(null, 'Success, Data', function () {});
+  t.deepEqual(lastSent, {type: 'done', msg: 'Success, Data'}, 'properly sends "data" message');
 
-  worker.done('Error Occurred', null);
+  w.done('Error Occurred', null, function () {});
   t.deepEqual(lastSent, {
     type: 'error',
     msg: {
@@ -44,72 +37,68 @@ test('work-handler -- `done` sends data messages', function (t) {
 test('work-handler -- on message, map is called', function (t) {
   var mapCalled = 0;
   var mappedData = null;
-  var custom = worker.extend({
-    map: function (data, w) {
-      mapCalled++;
-      mappedData = data;
-      w.done();
-    }
+  var custom = worker(function (data, enc, done) {
+    mapCalled++;
+    mappedData = data;
+    done();
   });
+
   var bus = new Emitter();
-  bus.send = function (message) {
-    if (message.type === 'doneBatch') {
+  bus.send = function (message, cb) {
+    if (message.type === 'done') {
       t.equal(mapCalled, 1, 'map should have been called once');
       t.equal(mappedData, '42!', 'map was passed original message that was emitted');
       t.end();
     }
+    if (!isOldNode) cb();
   };
   custom._install(bus, {}, {});
-  bus.emit('message', ['42!']);
+  bus.emit('message', '42!');
 });
 
 test('work-handler -- on map error, error message is sent via bus', function (t) {
   var e = new Error('This is the error message');
-  var custom = worker.extend({
-    map: function () {
-      throw e;
-    }
+  var custom = worker(function () {
+    throw e;
   });
 
   var bus = new Emitter();
-  bus.send = function (message) {
+  bus.send = function (message, cb) {
     if (message.type === 'error') {
       t.deepEqual({type: 'error', msg: {message: e.toString(), stack: e.stack}}, message, 'proper error message is propagated to bus');
+      if (!isOldNode) cb();
       t.end();
     }
   };
 
   custom._install(bus, {}, {});
-  custom.callMap(['some data']);
+  custom.callMap('some data');
 });
 
 test('work-handler -- push sends push data via bus', function (t) {
   var pushes = [];
-  var custom = worker.extend({
-    map: function (data, w) {
-      w.push(1);
-      w.push({value: 2});
-      w.done();
-    }
+  var custom = worker(function (data, enc, done) {
+    this.push(1);
+    this.push({value: 2});
+    done();
   });
 
   var bus = new Emitter();
-  bus.send = function (message) {
+  bus.send = function (message, cb) {
     if (message.type === 'push') {
       pushes.push(message);
     }
-    if (message.type === 'data') {
-      t.equal(pushes.length, 2, 'two items were pushed');
+    if (message.type === 'done') {
       t.deepEqual([
         {type: 'push', msg: 1},
         {type: 'push', msg: {value: 2}}
       ], pushes, 'the correct items were pushed');
-    }
-    if (message.type === 'doneBatch') {
+      t.equal(pushes.length, 2, 'two items were pushed');
       t.end();
     }
+    if (!isOldNode) cb();
   };
 
   custom._install(bus, {}, {});
-  custom.callMap(['some data']);
+  custom.callMap('some data', function () {});
 });
